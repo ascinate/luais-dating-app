f4a69a60e3de87f0da06356bb1585f090e2dccec
Pritam Ghosh <pritam7599@gmail.com>
2026-02-25 15:59:41 +0530

    explore

--- app/nearby.jsx
+++ app/nearby.jsx
@@ -1,33 +1,102 @@
-import { View, Text,StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 import CircleButton from '@/components/ui/CircleButton'
-import { Settings2, TableOfContents } from 'lucide-react-native';
+import BackgroundUI from '@/components/ui/BackgroundUI'
+import { MapPin, MessageCircleMore, Settings2, TableOfContents } from 'lucide-react-native';
+import ToggleButton from '@/components/ui/ToggleButton'
+import Paragraph from '@/components/ui/Paragraph'
+import CustomText from '@/components/ui/CustomText';
+import BottomTabs from '@/components/ui/BottomTabs';
+import { router } from 'expo-router'
 
 
 const nearby = () => {
     return (
-        <SafeAreaView>
+        <BackgroundUI>
+            <View style={{ margin: 12 }}>
+                <View style={styles.header}>
+                    <CircleButton Icon={TableOfContents} onPress={() => router.push('/userprofile')} />
+                    <ToggleButton />
+                    <CircleButton Icon={Settings2} onPress={() => router.push('/match')} />
+                </View>
 
-            <View style={styles.header}>
-                <CircleButton Icon={TableOfContents} onPress={() => router.push('/userprofile')} />
-           
-                <CircleButton Icon={Settings2} />
+
+
+                <Image
+                    source={require('../assets/images/map.png')}
+                />
+
+                <View style={{ borderWidth: 1, borderColor: '#585858', backgroundColor: '#252525', padding: 20, borderRadius: 25 }}>
+
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', }}>
+                        <View>
+                            <Image
+                                source={require('../assets/images/profile.jpg')}
+                                style={{ width: 60, height: 60, borderRadius: 50 }}
+                            />
+                        </View>
+                        <View>
+                            <View style={{ flexDirection: 'row' }}>
+                                <CustomText style={styles.profileName}>Aria Bennett,</CustomText>
+                                <CustomText style={[styles.profileName, { color: '#FF5F8A' }]}>23</CustomText>
+                            </View>
+                            <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                                <MapPin color="#fff" size={16} />
+                                <Paragraph style={{ marginLeft: 10 }}>USA NEW YORK</Paragraph>
+                            </View>
+
+                        </View>
+                        <TouchableOpacity>
+                            <MessageCircleMore color='#fff' />
+                        </TouchableOpacity>
+
+
+                    </View>
+                    <View style={{ flexDirection: 'row', gap: 6, marginTop: 10 }}>
+                        <View style={styles.badge}>
+                            <Paragraph>2.5km</Paragraph>
+                        </View>
+                        <View style={styles.badge}>
+                            <Paragraph>2.5km</Paragraph>
+                        </View>
+                        <View style={styles.badge}>
+                            <Paragraph>2.5km</Paragraph>
+                        </View>
+                    </View>
+
+
+                </View>
+                <View style={{ alignItems: 'center', marginTop: 10 }}>
+                    <BottomTabs />
+                </View>
+
             </View>
 
-
-            <View>
-                <Text>nearby</Text>
-            </View>
-        </SafeAreaView>
+        </BackgroundUI>
     )
 }
-
 export default nearby;
 
 
 const styles = StyleSheet.create({
-
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        borderRadius: 25
+    },
+    profileName: {
+        fontSize: 20,
+        color: '#fff'
+    },
+    badge: {
+        backgroundColor: '#373737',
+        width: 78,
+        height: 38,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 50
+    }
 })
